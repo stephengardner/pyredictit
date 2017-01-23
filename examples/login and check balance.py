e59835b480c68f97ec6ec77188118e3420bde896
Nick Tucker <ntucker1@ithaca.edu>
2017-01-23 16:27:22 -0500

    Create login and check balance.py

---
+++ examples/login and check balance.py
@@ -0,0 +1,7 @@
+from pyredictit.pyredictit import pyredictit
+
+pyredictit_api = pyredictit()
+pyredictit_api.create_authed_session(username='YOUR_USERNAME',
+                                     password='YOUR_PASSWORD')
+pyredictit_api.current_gain_loss()
+>>> $0.00
